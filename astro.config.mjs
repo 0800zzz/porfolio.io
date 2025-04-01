2a0623cbf0bb0b431be1842efe83902bdff3d398
0800zzz <carrizovergarae@gmail.com>
2025-04-01 19:34:24 -0300

    edicion y creacion de archivos importantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://0800zzz.github.io',
-  base: 'astro_github_pages_ejemplo',
-})
+  base: '/porfolio.io/'  // ¡Debe coincidir con tu repo!
+});
