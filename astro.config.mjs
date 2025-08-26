925595d2d94ffa5e3eafc3d6007f2995cd2b32e3
0800zzz <carrizovergarae@gmail.com>
2025-08-26 05:30:57 -0300

    chore: remove mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://0800zzz.github.io',
   base: '/porfolio.io',
   output: 'static',
-  integrations: [mdx()],   // <— CLAVE
+  // integrations: []  // <- SIN mdx
 });
